ca8c5190bebec0dab3c1abf8e59046462613cc53
andrei <andrei>
2001-09-21 21:42:52 +0000

    - added test configs & packets

--- sip_router/route.c
+++ sip_router/route.c
@@ -1,5 +1,5 @@
 /*
- * $Id: route.c,v 1.8 2001/09/21 20:47:18 andrei Exp $
+ * $Id: route.c,v 1.9 2001/09/21 21:42:52 andrei Exp $
  *
  * SIP routing engine
  *
@@ -235,6 +235,7 @@ static int comp_ip(unsigned a, void* param, int op, int subtype)
 			ret=(a&((struct net*)param)->mask)==((struct net*)param)->ip;
 			break;
 		case STRING_ST:
+		case RE_ST:
 			/* 1: compare with ip2str*/
 			ret=comp_str(inet_ntoa(*(struct in_addr*)&a), param, op,
 						subtype);
