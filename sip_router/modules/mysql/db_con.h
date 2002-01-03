88fac0dabe1678094e31cef422dc1e48253f30c9
janakj <janakj>
2002-01-03 15:19:40 +0000

    *** empty log message ***

--- sip_router/modules/mysql/db_con.h
+++ sip_router/modules/mysql/db_con.h
@@ -4,7 +4,7 @@
 /*
  * Database connection data type for database module
  *
- * $Id: db_con.h,v 1.1 2001/12/18 00:28:45 janakj Exp $
+ * $Id: db_con.h,v 1.2 2002/01/03 15:19:40 janakj Exp $
  */
 
 #include <mysql/mysql.h>
@@ -15,18 +15,18 @@
  * handle
  */
 typedef struct {
-	const char* table;    /* Default table to use */
-	MYSQL       con;      /* Mysql Connection */
+	char* table;          /* Default table to use */
+	MYSQL*      con;      /* Mysql Connection */
 	MYSQL_RES*  res;      /* Result of previous operation */
 	MYSQL_ROW   row;      /* Actual row in the result */
 	int         connected;
 } db_con_t;
 
-
-#define CONNECTED(con) do {                                  \
-                            return (con->connected == TRUE); \
-                          } while(0)
-
+#define CON_CONNECTED(cn) ((cn)->connected)
+#define CON_TABLE(cn) ((cn)->table)
+#define CON_CONNECTION(cn) ((cn)->con)
+#define CON_RESULT(cn) ((cn)->res)
+#define CON_ROW(cn) ((cn)->row)
 
 int use_table(db_con_t* _h, const char* _t);
 
