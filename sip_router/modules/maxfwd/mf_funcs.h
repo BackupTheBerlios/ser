eca08b9b763d8a9200a7f9e195497ba9a9ffe731
bogdan <bogdan>
2002-05-15 13:43:49 +0000

    Max-Forwards proccessing fixed

--- sip_router/modules/maxfwd/mf_funcs.h
+++ sip_router/modules/maxfwd/mf_funcs.h
@@ -1,5 +1,5 @@
 /*
- * $Id: mf_funcs.h,v 1.6 2002/05/13 01:15:40 jku Exp $
+ * $Id: mf_funcs.h,v 1.7 2002/05/15 13:43:49 bogdan Exp $
  */
 
 #ifndef _MF_FUNCS_H
@@ -9,14 +9,12 @@
 #include "../../parser/msg_parser.h"
 #include "../../dprint.h"
 #include "../../config.h"
+#include "../../str.h"
 
 
-int mf_startup();
-int decrement_maxfwd( struct sip_msg* msg );
+int decrement_maxfwd( struct sip_msg* msg, int nr_val, str *str_val );
 int add_maxfwd_header( struct sip_msg* msg , unsigned int val );
-int is_maxfwd_zero( struct sip_msg* msg );
-int reply_to_maxfwd_zero( struct sip_msg* msg );
-int is_maxfwd_present( struct sip_msg* msg );
+int is_maxfwd_present( struct sip_msg* msg, str *mf_value );
 
 #endif
 
