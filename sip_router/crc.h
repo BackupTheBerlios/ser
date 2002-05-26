6abb638d2f75f5c45cabe4255a278daee5589e5a
jku <jku>
2002-05-26 17:53:21 +0000

    added variable to-rag part to mitigate dropping TM-owned ACKs

--- sip_router/crc.h
+++ sip_router/crc.h
@@ -1,11 +1,16 @@
-/* $Id: crc.h,v 1.1 2002/01/07 04:39:49 jku Exp $*/
+/* $Id: crc.h,v 1.2 2002/05/26 17:53:21 jku Rel $*/
 
 #ifndef _CRC_H_
 #define _CRC_H_
 
+#define CRC16_LEN	4
+
 extern unsigned long int crc_32_tab[];
 extern unsigned short int ccitt_tab[];
 extern unsigned short int crc_16_tab[];
+
+unsigned short crcitt_string( char *s, int len );
+void crcitt_string_array( char *dst, str src[], int size );
 
 #endif
 
--- sip_router/modules/sl/sl_funcs.h
+++ sip_router/modules/sl/sl_funcs.h
@@ -1,5 +1,5 @@
 /*
- * $Id: sl_funcs.h,v 1.3 2002/05/13 01:15:41 jku Exp $
+ * $Id: sl_funcs.h,v 1.4 2002/05/26 17:53:21 jku Exp $
  */
 
 #ifndef _SL_FUNCS_H
@@ -7,7 +7,11 @@
 
 #include "../../parser/msg_parser.h"
 
+#define TOTAG_SEPARATOR		'.'
+
 #define SL_RPL_WAIT_TIME  2  // in sec
+
+#define TOTAG_LEN MD5_LEN+CRC16_LEN+1
 
 int sl_startup();
 int sl_send_reply(struct sip_msg*,int,char*);
