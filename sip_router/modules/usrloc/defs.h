fec279cd559cf45be76f6044624a5207a05d1e4c
janakj <janakj>
2002-06-10 17:02:53 +0000

    Out-Of-Order hack

--- sip_router/modules/usrloc/defs.h
+++ sip_router/modules/usrloc/defs.h
@@ -1,5 +1,5 @@
 /*
- * $Id: defs.h,v 1.9 2002/05/11 21:31:48 jku Exp $
+ * $Id: defs.h,v 1.10 2002/06/10 17:02:53 janakj Exp $
  */
 
 #ifndef DEFS_H
@@ -8,5 +8,12 @@
 #define MAX_CONTACT_BUFFER 1024
 
 #define DEFAULT_CACHE_SIZE 512
+
+/*
+ * Needed for some broken clients, if defined,
+ * registers with the same callid but lower cseq
+ * will be accepted
+ */
+#define OOO_HACK
 
 #endif
