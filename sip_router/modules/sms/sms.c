2ec53c377a2da2bee8397751ae60ee74bd2f9de1
bogdan <bogdan>
2002-07-01 15:15:42 +0000

    *** empty log message ***

--- sip_router/modules/sms/sms.c
+++ sip_router/modules/sms/sms.c
@@ -1,5 +1,5 @@
 /*
- * $Id: sms.c,v 1.12 2002/07/01 14:56:49 bogdan Exp $
+ * $Id: sms.c,v 1.13 2002/07/01 15:15:42 bogdan Exp $
  *
  * MAXFWD module
  *
@@ -533,11 +533,13 @@ int global_init()
 			LOG(L_ERR,"ERROR: sms_global_init: cannot create pipe!\n");
 			goto error;
 		}
+		DBG("DEBUG: sms_global_init : pipe for %s is (%d,%d)\n",
+			networks[i].name, net_pipe[0], net_pipe[1] );
 		networks[i].pipe_out = net_pipe[0];
 		net_pipes_in[i] = net_pipe[1];
 		/* sets reading from pipe to non blocking */
 		if ((foo=fcntl(net_pipe[0],F_GETFL,0))<0) {
-			LOG(L_ERR,"ERROR: sms_blobal_init: cannot get flag for pipe"
+			LOG(L_ERR,"ERROR: sms_global_init: cannot get flag for pipe"
 				" - fcntl\n");
 			goto error;
 		}
