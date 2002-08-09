33ffc171bf9a92d530e074fcc150164c451fa135
janakj <janakj>
2002-08-09 13:24:58 +0000

    db doc updated

--- sip_router/db/db.c
+++ sip_router/db/db.c
@@ -1,5 +1,5 @@
 /*
- * $Id: db.c,v 1.2 2002/07/09 09:16:05 janakj Exp $
+ * $Id: db.c,v 1.3 2002/08/09 13:24:58 janakj Exp $
  */
 
 #include "db.h"
@@ -13,25 +13,25 @@ int bind_dbmod(void)
 	dbf.use_table = (db_use_table_f)find_export("db_use_table", 2);
 
 	dbf.init = (db_init_f)find_export("db_init", 1);
-	if (!dbf.init) return 1;
+	if (!dbf.init) return -1;
 
 	dbf.close = (db_close_f)find_export("db_close", 2);
-	if (!dbf.close) return 1;
+	if (!dbf.close) return -1;
 
 	dbf.query = (db_query_f)find_export("db_query", 2);
-	if (!dbf.query) return 1;
+	if (!dbf.query) return -1;
 
 	dbf.free_query = (db_free_query_f)find_export("db_free_query", 2);
-	if (!dbf.free_query) return 1;
+	if (!dbf.free_query) return -1;
 
 	dbf.insert = (db_insert_f)find_export("db_insert", 2);
-	if (!dbf.insert) return 1;
+	if (!dbf.insert) return -1;
 
 	dbf.delete = (db_delete_f)find_export("db_delete", 2);
-	if (!dbf.delete) return 1;
+	if (!dbf.delete) return -1;
 
 	dbf.update = (db_update_f)find_export("db_update", 2);
-	if (!dbf.update) return 1;
+	if (!dbf.update) return -1;
 
 	return 0;
 }
