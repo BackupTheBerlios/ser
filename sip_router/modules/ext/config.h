ee40127aa745423535243f0fc3661ddaa6629dc8
jku <jku>
2002-08-15 14:08:59 +0000

    moving all config stuff into a separate dir

---
+++ sip_router/modules/ext/config.h
@@ -0,0 +1,14 @@
+/*
+ *
+ * $Id: config.h,v 1.1 2002/08/15 14:08:59 jku Exp $
+ *
+ */
+
+#ifndef _EXT_H
+
+#define MAX_PIPE_BUFFER_LEN 1024
+#define SHELL "/bin/sh"
+#define SHELL_NAME "sh"
+#define SHELL_PARAM "-c"
+
+#endif
