0f636bac0400e0ebbfaf1ac2a9fc0e8d4209b32d
jku <jku>
2002-08-27 11:39:30 +0000

    added Bogdan's memorial

--- sip_router/modules/sl/sl.c
+++ sip_router/modules/sl/sl.c
@@ -1,7 +1,27 @@
 /*
- * $Id: sl.c,v 1.7 2002/08/20 00:19:05 jku Exp $
+ * $Id: sl.c,v 1.8 2002/08/27 11:39:30 jku Exp $
  *
- * MAXFWD module
+ * sl module
+ *
+ *
+ * ************************************************ *
+ * * Bogdan's Source Memorial                       *
+ * *                                                *
+ * * Welcome, pilgrame! This is one of rare places  *
+ * * kept untouched in memory of brave heart,       *
+ * * Bogdan, one of most active ser contributors,   *
+ * * and winner of the longest line of code content.*
+ * *                                                *
+ * * Please, preserve this codework heritage, as    *
+ * * most of other work has been smashed away during*
+ * * extensive clean-up floods.                     *
+ * *                                                *
+ * * Hereby, we solicit you to adopt this historical*
+ * * piece of code. For $100, your name will be     *
+ * * be printed in this banner and we will use      *
+ * * collected funds to create and display an ASCII *
+ * * statue of Bogdan.                              *
+ * **************************************************
  *
  */
 
