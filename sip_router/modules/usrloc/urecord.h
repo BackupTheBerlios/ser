cc1613cf6872b57904e8eab154b17fe334b0c9d3
janakj <janakj>
2002-08-29 21:03:15 +0000

    *** empty log message ***

--- sip_router/modules/usrloc/urecord.h
+++ sip_router/modules/usrloc/urecord.h
@@ -1,5 +1,5 @@
 /* 
- * $Id: urecord.h,v 1.5 2002/08/27 13:31:25 janakj Exp $ 
+ * $Id: urecord.h,v 1.6 2002/08/29 21:03:15 janakj Exp $ 
  *
  * Usrloc record structure
  */
@@ -59,8 +59,15 @@ int mem_insert_ucontact(urecord_t* _r, str* _c, time_t _e, float _q,
 			str* _cid, int _cs, struct ucontact** _con);
 
 
+
 /*
- * Remove contact from the list
+ * Remove the contact from lists
+ */
+void mem_remove_ucontact(urecord_t* _r, ucontact_t* _c);
+
+
+/*
+ * Remove contact from the list and delete 
  */
 void mem_delete_ucontact(urecord_t* _r, ucontact_t* _c);
 
