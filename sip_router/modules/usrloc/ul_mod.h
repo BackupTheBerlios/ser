4082a93cc84dde5a1ce22170d148339b0d10f099
janakj <janakj>
2002-12-10 20:25:37 +0000

    username and domain stored in separate columns, internally represented as a single string of form <username@domain>, this will change in the future. For now it is the easiest way how to do it.

--- sip_router/modules/usrloc/ul_mod.h
+++ sip_router/modules/usrloc/ul_mod.h
@@ -1,5 +1,5 @@
 /*
- * $Id: ul_mod.h,v 1.6 2002/12/10 19:52:20 janakj Exp $
+ * $Id: ul_mod.h,v 1.7 2002/12/10 20:25:37 janakj Exp $
  *
  * Usrlocation module interface
  *
@@ -46,6 +46,7 @@
 
 
 extern char* user_col;
+extern char* domain_col;
 extern char* contact_col;
 extern char* expires_col;
 extern char* q_col;
@@ -55,7 +56,7 @@ extern char* method_col;
 extern char* db_url;
 extern int   timer_interval;
 extern int   db_mode;
-extern int   use_mode;
+extern int   use_domain;
 
 extern db_con_t* db;   /* Dabase connection handle */
 
