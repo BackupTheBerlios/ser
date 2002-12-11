6679c6a11c30501e305310c7e389c720f877f222
janakj <janakj>
2002-12-11 14:02:20 +0000

    Added.

---
+++ sip_router/modules/rr/rr_mod.h
@@ -0,0 +1,14 @@
+/*
+ * Record-Route & Route module interface
+ *
+ * $Id: rr_mod.h,v 1.1 2002/12/11 14:02:20 janakj Exp $
+ */
+
+#ifndef RR_MOD_H
+#define RR_MOD_H
+
+
+extern int use_fast_cmp;
+
+
+#endif /* RR_MOD_H */
