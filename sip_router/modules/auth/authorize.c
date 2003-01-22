1f6459a5977c1665fe9e1ff72d20a200113a418b
janakj <janakj>
2003-01-22 00:45:13 +0000

    Changed the way how consume_credentials calculates header field length.

--- sip_router/modules/auth/authorize.c
+++ sip_router/modules/auth/authorize.c
@@ -1,5 +1,5 @@
 /*
- * $Id: authorize.c,v 1.14 2003/01/16 00:25:20 janakj Exp $
+ * $Id: authorize.c,v 1.15 2003/01/22 00:45:13 janakj Exp $
  *
  * Authorize related functions
  *
@@ -431,9 +431,8 @@ int consume_credentials(struct sip_msg* _m, char* _s1, char* _s2)
 		}
 	}
 
-	len = h->name.len + h->body.len + 2;
-	if (h->body.s[h->body.len] != '\0') len++; 
-	/* FIXME: Is this necessary ?, Yes, it is */
+	if (h->next) len = h->next->name.s - h->name.s;
+	else len = _m->unparsed - h->name.s;
 
 	if (del_lump(&_m->add_rm, h->name.s - _m->buf, len, 0) == 0) {
 		LOG(L_ERR, "consume_credentials(): Can't remove credentials\n");
