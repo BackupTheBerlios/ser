30835041d5a6593124454289218ea086e32f4362
janakj <janakj>
2003-01-23 23:07:06 +0000

    primary key of subscribers and pending tables is pair (user_id, domain).

--- sip_router/scripts/ser_mysql.sh
+++ sip_router/scripts/ser_mysql.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 #
-# $Id: ser_mysql.sh,v 1.24 2003/01/23 22:45:49 janakj Exp $
+# $Id: ser_mysql.sh,v 1.25 2003/01/23 23:07:06 janakj Exp $
 #
 # Script for adding and dropping ser MySql tables
 # 
@@ -289,7 +289,7 @@ CREATE TABLE pending (
   perms varchar(32) default NULL,
   allow_find char(1) NOT NULL default '0',
   timezone varchar(128) default NULL,
-  UNIQUE KEY USER_ID (USER_ID),
+  PRIMARY KEY (USER_ID, domain),
   KEY USER_ID_2 (USER_ID),
   UNIQUE KEY phplib_id (phplib_id)
 ) $TABLE_TYPE;
@@ -355,7 +355,7 @@ CREATE TABLE subscriber (
   allow_find char(1) NOT NULL default '0',
   timezone varchar(128) default NULL,
   UNIQUE KEY phplib_id (phplib_id),
-  UNIQUE KEY USER_ID (USER_ID),
+  PRIMARY KEY (USER_ID, domain),
   KEY USER_ID_2 (USER_ID)
 ) $TABLE_TYPE;
 
