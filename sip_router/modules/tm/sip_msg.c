b704d3bba57d356a6ee6c0e943fbeb050f0758e3
janakj <janakj>
2003-03-06 15:53:05 +0000

    Reflected changes in the digest parser.

--- sip_router/modules/tm/sip_msg.c
+++ sip_router/modules/tm/sip_msg.c
@@ -1,5 +1,5 @@
 /*
- * $Id: sip_msg.c,v 1.66 2003/03/04 14:38:31 janakj Exp $
+ * $Id: sip_msg.c,v 1.67 2003/03/06 15:55:19 janakj Exp $
  * 
  * cloning a message into shared memory (TM keeps a snapshot
  * of messages in memory); note that many operations, which
@@ -166,10 +166,9 @@ static inline struct auth_body* auth_body_cloner(char* new_buf, char *org_buf, s
 	(*p) += ROUND4(sizeof(struct auth_body));
 	
 	     /* authorized field must be cloned elsewhere */
-	new_auth->digest.username.s = translate_pointer(new_buf, org_buf, auth->digest.username.s);
-#ifdef DIGEST_DOMAIN
-	new_auth->digest.domain.s = translate_pointer(new_buf, org_buf, auth->digest.domain.s);
-#endif
+	new_auth->digest.username.whole.s = translate_pointer(new_buf, org_buf, auth->digest.username.whole.s);
+	new_auth->digest.username.user.s = translate_pointer(new_buf, org_buf, auth->digest.username.user.s);
+	new_auth->digest.username.domain.s = translate_pointer(new_buf, org_buf, auth->digest.username.domain.s);
 	new_auth->digest.realm.s = translate_pointer(new_buf, org_buf, auth->digest.realm.s);
 	new_auth->digest.nonce.s = translate_pointer(new_buf, org_buf, auth->digest.nonce.s);
 	new_auth->digest.uri.s = translate_pointer(new_buf, org_buf, auth->digest.uri.s);
