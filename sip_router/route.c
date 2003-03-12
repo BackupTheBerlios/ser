0d5cb441849495f79a8db5dd06fc94a47f7397e1
andrei <andrei>
2003-03-12 15:55:28 +0000

    - minor warnings fixes (gcc)

--- sip_router/route.c
+++ sip_router/route.c
@@ -1,5 +1,5 @@
 /*
- * $Id: route.c,v 1.30 2003/03/10 22:24:14 andrei Exp $
+ * $Id: route.c,v 1.31 2003/03/12 15:55:28 andrei Exp $
  *
  * SIP routing engine
  *
@@ -141,7 +141,7 @@ static int fix_actions(struct action* a)
 	struct action *t;
 	struct proxy_l* p;
 	char *tmp;
-	int ret,r;
+	int ret;
 	cmd_export_t* cmd;
 	struct sr_module* mod;
 	str s;
