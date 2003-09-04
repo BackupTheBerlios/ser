589d84fc3403f1a4930d48aa9b3b03d9e9db84c0
bogdan <bogdan>
2003-09-04 11:21:47 +0000

    -some updates required by libxml->libxml2 translation -some warnings fixed

--- sip_router/modules/cpl-c/cpl_time.c
+++ sip_router/modules/cpl-c/cpl_time.c
@@ -1,5 +1,5 @@
 /*
- * $Id: cpl_time.c,v 1.3 2003/09/02 16:08:57 bogdan Exp $
+ * $Id: cpl_time.c,v 1.4 2003/09/04 11:21:47 bogdan Exp $
  *
  * Copyright (C) 2001-2003 Fhg Fokus
  *
@@ -394,7 +394,7 @@ int tr_parse_dtstart(tmrec_p _trp, char *_in)
 	if(!_trp || !_in)
 		return -1;
 	_trp->dtstart = ic_parse_datetime(_in, &(_trp->ts));
-	DBG("----->dtstart = %d | %s\n",_trp->dtstart,ctime(&(_trp->dtstart)));
+	DBG("----->dtstart = %ld | %s\n",_trp->dtstart,ctime(&(_trp->dtstart)));
 	return (_trp->dtstart==0)?-1:0;
 }
 
@@ -404,7 +404,7 @@ int tr_parse_dtend(tmrec_p _trp, char *_in)
 	if(!_trp || !_in)
 		return -1;
 	_trp->dtend = ic_parse_datetime(_in,&_tm);
-	DBG("----->dtend = %d | %s\n",_trp->dtend,ctime(&(_trp->dtend)));
+	DBG("----->dtend = %ld | %s\n",_trp->dtend,ctime(&(_trp->dtend)));
 	return (_trp->dtend==0)?-1:0;
 }
 
