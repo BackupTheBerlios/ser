b9c6f9e452bd78137eb665de5f6424cbcdd74abc
bogdan <bogdan>
2003-09-12 15:48:17 +0000

    - when doing redirect, the msg which I'm working with can be in private memory   or is a clone into shared memory (if I'm after a failed proxy); So, it's   better to removed by myself the lump after calling t_reply()

--- sip_router/modules/cpl-c/cpl_run.c
+++ sip_router/modules/cpl-c/cpl_run.c
@@ -1,5 +1,5 @@
 /*
- * $Id: cpl_run.c,v 1.22 2003/09/11 22:33:32 bogdan Exp $
+ * $Id: cpl_run.c,v 1.23 2003/09/12 15:48:17 bogdan Exp $
  *
  * Copyright (C) 2001-2003 Fhg Fokus
  *
@@ -589,6 +589,13 @@ static inline char *run_redirect( struct cpl_interpreter *intr )
 		i = cpl_tmb.t_reply( intr->msg, (int)301, "Moved permanently" );
 	else
 		i = cpl_tmb.t_reply( intr->msg, (int)302, "Moved temporarily" );
+
+	/* msg which I'm working with can be in private memory or is a clone into
+	 * shared memory (if I'm after a failed proxy); So, it's better to removed
+	 * by myself the lump that I added previosly */
+	unlink_lump_rpl( intr->msg, lump);
+	free_lump_rpl( lump );
+
 	if (i!=1) {
 		LOG(L_ERR,"ERROR:cpl-c:run_redirect: unable to send redirect reply!\n");
 		goto runtime_error;
