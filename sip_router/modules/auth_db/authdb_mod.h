eba9d624c0b3cbe46bc0c138ab600c3c90b4052f
janakj <janakj>
2003-11-29 00:35:51 +0000

    - check for database table version

--- sip_router/modules/auth_db/authdb_mod.h
+++ sip_router/modules/auth_db/authdb_mod.h
@@ -1,5 +1,5 @@
 /*
- * $Id: authdb_mod.h,v 1.4 2003/09/15 19:47:02 janakj Exp $
+ * $Id: authdb_mod.h,v 1.5 2003/11/29 00:35:51 janakj Exp $
  *
  * Digest Authentication - Database support
  *
@@ -31,6 +31,7 @@
 #ifndef AUTHDB_MOD_H
 #define AUTHDB_MOD_H
 
+#include "../../str.h"
 #include "../../db/db.h"
 #include "../auth/api.h"
 #include "../../parser/msg_parser.h"
@@ -40,14 +41,14 @@
  * Module parameters variables
  */
 
-extern char* db_url;          /* Database URL */
-extern char* user_column;     /* 'username' column name */
-extern char* domain_column;   /* 'domain' column name */
-extern char* rpid_column;     /* 'rpid' column name */
-extern char* pass_column;     /* 'password' column name */
-extern char* pass_column_2;   /* Column containg HA1 string constructed
-			       * of user@domain username
-			       */
+extern str db_url;          /* Database URL */
+extern str user_column;     /* 'username' column name */
+extern str domain_column;   /* 'domain' column name */
+extern str rpid_column;     /* 'rpid' column name */
+extern str pass_column;     /* 'password' column name */
+extern str pass_column_2;   /* Column containg HA1 string constructed
+			     * of user@domain username
+			     */
 
 extern int calc_ha1;          /* if set to 1, ha1 is calculated by the server */
 extern int use_domain;        /* If set to 1 then the domain will be used when selecting a row */
--- sip_router/modules/auth_db/authorize.c
+++ sip_router/modules/auth_db/authorize.c
@@ -1,5 +1,5 @@
 /*
- * $Id: authorize.c,v 1.12 2003/11/05 03:49:32 janakj Exp $
+ * $Id: authorize.c,v 1.13 2003/11/29 00:35:51 janakj Exp $
  *
  * Digest Authentication - Database support
  *
@@ -60,10 +60,10 @@ static inline int get_ha1(struct username* _username, str* _domain, char* _table
 	str result;
 	int n, nc;
 
-	keys[0] = user_column;
-	keys[1] = domain_column;
-	col[0] = (_username->domain.len && !calc_ha1) ? (pass_column_2) : (pass_column);	
-	col[1] = rpid_column;
+	keys[0] = user_column.s;
+	keys[1] = domain_column.s;
+	col[0] = (_username->domain.len && !calc_ha1) ? (pass_column_2.s) : (pass_column.s);	
+	col[1] = rpid_column.s;
 
 	VAL_TYPE(vals) = VAL_TYPE(vals + 1) = DB_STR;
 	VAL_NULL(vals) = VAL_NULL(vals + 1) = 0;
