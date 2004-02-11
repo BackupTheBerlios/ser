b8bb32d2a9096bb3c9cb2e86a3da197cfd575ac7
bogdan <bogdan>
2004-02-11 11:27:58 +0000

    - bug fixed - all calbacks return as parameter a pointer to the registered   param, instead of the param itself (to allow you to modify the param).   looks like fifo_callback wasn't updated (probably my fault)

--- sip_router/modules/tm/uac_fifo.c
+++ sip_router/modules/tm/uac_fifo.c
@@ -1,5 +1,5 @@
 /*
- * $Id: uac_fifo.c,v 1.8 2004/02/11 10:44:32 bogdan Exp $
+ * $Id: uac_fifo.c,v 1.9 2004/02/11 11:27:58 bogdan Exp $
  *
  * Copyright (C) 2001-2003 Fhg Fokus
  *
@@ -541,12 +541,12 @@ static void fifo_callback( struct cell *t, int type, struct tmcb_params *ps )
 	DBG("!!!!! ref_counter: %d\n", t->ref_count);
 
 	DBG("DEBUG: fifo UAC completed with status %d\n", ps->code);
-	if (!ps->param) {
+	if (!*ps->param) {
 		LOG(L_INFO, "INFO: fifo UAC completed with status %d\n", ps->code);
 		return;
 	}
 
-	filename=(char *)(ps->param);
+	filename=(char *)(*ps->param);
 	if (ps->rpl==FAKED_REPLY) {
 		get_reply_status( &text, ps->rpl, ps->code);
 		if (text.s==0) {
@@ -570,7 +570,7 @@ static void fifo_callback( struct cell *t, int type, struct tmcb_params *ps )
 	}
 	DBG("DEBUG: fifo_callback sucesssfuly completed\n");
 done:
-	shm_free(ps->param);
+	shm_free(filename);
 }
 
 
