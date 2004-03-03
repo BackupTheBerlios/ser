b8bf26abfe378d4c3ee972f1642e92032fd63fab
bogdan <bogdan>
2004-03-03 13:09:58 +0000

    - in failure route, msg->parsed_uri_ok must be reset!!! since msg_parsed_uri is     not cloned (and cannot be cloned)!

--- sip_router/modules/tm/t_reply.c
+++ sip_router/modules/tm/t_reply.c
@@ -1,5 +1,5 @@
 /*
- * $Id: t_reply.c,v 1.81 2004/02/23 16:47:43 bogdan Exp $
+ * $Id: t_reply.c,v 1.82 2004/03/03 13:09:58 bogdan Exp $
  *
  *
  * Copyright (C) 2001-2003 Fhg Fokus
@@ -457,6 +457,9 @@ static inline int fake_req(struct sip_msg *faked_req,
 	/* if we set msg_id to something different from current's message
 	 * id, the first t_fork will properly clean new branch URIs */
 	faked_req->id=shmem_msg->id-1;
+	/* msg->parsed_uri_ok must be reset since msg_parsed_uri is
+	 * not cloned (and cannot be cloned) */
+	faked_req->parsed_uri_ok = 0;
 
 	/* new_uri can change -- make a private copy */
 	if (shmem_msg->new_uri.s!=0 && shmem_msg->new_uri.len!=0) {
