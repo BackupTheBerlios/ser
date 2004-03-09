26da36946ba8cd62aed0397952d7b531420accca
sobomax <sobomax>
2004-03-09 14:45:12 +0000

    If SER compiled without USE_TCP don't try to lookup for a function t_relay_to_tcp. This fixes acc module loading when SER compiled without USE_TCP.

--- sip_router/modules/tm/tm_load.c
+++ sip_router/modules/tm/tm_load.c
@@ -1,5 +1,5 @@
 /*
- * $Id: tm_load.c,v 1.18 2004/01/22 14:28:50 bogdan Exp $
+ * $Id: tm_load.c,v 1.19 2004/03/09 14:45:12 sobomax Exp $
  *
  * Copyright (C) 2001-2003 Fhg Fokus
  *
@@ -49,10 +49,12 @@ int load_tm( struct tm_binds *tmb)
 		LOG(L_ERR, LOAD_ERROR "'t_newtran' not found\n");
 		return -1;
 	}
+#ifdef USE_TCP
 	if (!( tmb->t_relay_to_tcp=find_export(T_RELAY_TO_TCP, 2, 0)) ) {
 		LOG(L_ERR, LOAD_ERROR "'t_relay_to_tcp' not found\n");
 		return -1;
 	}
+#endif
 	if (!( tmb->t_relay_to_udp=find_export(T_RELAY_TO_UDP, 2, 0)) ) {
 		LOG(L_ERR, LOAD_ERROR "'t_relay_to_udp' not found\n");
 		return -1;
