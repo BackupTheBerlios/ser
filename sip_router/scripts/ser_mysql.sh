e85003a42122bc30b5026197439f6ff47b2ac61d
janakj <janakj>
2004-04-25 19:48:06 +0000

    A regular expression matching user does not match username anymore (caused wrong expansion of username to usernamename)

--- sip_router/scripts/ser_mysql.sh
+++ sip_router/scripts/ser_mysql.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $Id: ser_mysql.sh,v 1.52 2004/04/13 19:14:32 kozlik Exp $
+# $Id: ser_mysql.sh,v 1.53 2004/04/25 19:48:06 janakj Exp $
 #
 # Script for adding and dropping ser MySql tables
 #
@@ -697,7 +697,7 @@ case $1 in
 			exit 1
 		fi
 		sed "s/[uU][sS][eE][rR]_[iI][dD]/user/g" $tmp_file |
-			sed "s/[uU][sS][eE][rR]/$USERCOL/g" |
+			sed "s/[uU][sS][eE][rR]\($\|[^a-zA-Z]\)/$USERCOL\1/g" |
 			sed "s/[rR][eE][aA][lL][mM]/domain/g"> ${tmp_file}.2
 		#3 drop original database
 		echo "dropping table ($DBNAME)"
