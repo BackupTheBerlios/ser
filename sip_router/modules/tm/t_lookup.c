cb7ce71f7a08deebe50c33b920930f920efa6e32
jiri <jiri>
2004-10-10 21:25:01 +0000

    Changed the way destination is determined for some packets if mhomed is turned on. That's particularly helpful in scenarios in which multiple IPs are used on the same interface and you wish to know in advance which IP will be used without knowledge of IP routing tables. - nathelper allows to bind to a specific IP address; that's   a plus if you have multiple IPs on the same interface and   wish to guarantee that an IP will be used which is not   default route (this overrides mhomed algorithm) - sl and tm send replies always back from socket where   request were received (now regardless of mhomed option)

--- sip_router/modules/tm/t_lookup.c
+++ sip_router/modules/tm/t_lookup.c
@@ -1,5 +1,5 @@
 /*
- * $Id: t_lookup.c,v 1.88 2004/08/24 09:00:42 janakj Exp $
+ * $Id: t_lookup.c,v 1.89 2004/10/10 21:25:01 jiri Exp $
  *
  * This C-file takes care of matching requests and replies with
  * existing transactions. Note that we do not do SIP-compliant
@@ -909,6 +909,7 @@ int init_rb( struct retr_buf *rb, struct sip_msg *msg)
 	struct socket_info* send_sock;
 	struct via_body* via;
 	int proto;
+	int backup_mhomed;
 
 	via=msg->via1;
 	if (!reply_to_via) {
@@ -926,7 +927,13 @@ int init_rb( struct retr_buf *rb, struct sip_msg *msg)
 	}
 	rb->dst.proto=proto;
 	rb->dst.proto_reserved1=msg->rcv.proto_reserved1;
+	/* turn off mhomed for generating replies -- they are ideally sent to where
+	   request came from to make life with NATs and other beasts easier
+	*/
+	backup_mhomed=mhomed;
+	mhomed=0;
 	send_sock=get_send_socket(&rb->dst.to, proto);
+	mhomed=backup_mhomed;
 	if (send_sock==0) {
 		LOG(L_ERR, "ERROR: init_rb: cannot fwd to af %d, proto %d "
 			"no socket\n", rb->dst.to.s.sa_family, proto);
