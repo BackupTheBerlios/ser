b5b79ba964ce47ed48ebfae17e0c690385fcc945
fgalan <fgalan>
2005-06-21 07:37:33 +0000

    Added warning banner

--- experimental/path/config.h
+++ experimental/path/config.h
@@ -1,4 +1,13 @@
 #ifndef _PATH_CONFIG_H
 #define _PATH_CONFIG_H
 
+#warning "============================================="
+#warning ""
+#warning "The path module is experimental man may crash"
+#warning "SER or create unexpected results. Use at own" 
+#warning "risk, but use it :)"
+#warning "Please submit bugs at http://bugs.sip-router.org/"
+#warning ""
+#warning "============================================="
+
 #endif
