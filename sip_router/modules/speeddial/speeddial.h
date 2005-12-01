f7f449158ca1e19fc2a7325c5fa9a9c2f47bb016
janakj <janakj>
2005-12-01 11:25:58 +0000

    - support for uid and did - updated to the latest db schema

--- sip_router/modules/speeddial/speeddial.h
+++ sip_router/modules/speeddial/speeddial.h
@@ -1,5 +1,5 @@
 /*
- * $Id: speeddial.h,v 1.1 2004/10/27 18:21:22 ramona Exp $
+ * $Id: speeddial.h,v 1.2 2005/12/01 11:25:58 janakj Exp $
  *
  * Copyright (C) 2004 Voice Sistem SRL
  *
@@ -39,13 +39,10 @@
 
 /* Module parameters variables */
 
-extern char* user_column;     /* 'username' column name */
-extern char* domain_column;   /* 'domain' column name */
-extern char* sd_user_column;     /* 'sd_username' column name */
-extern char* sd_domain_column;   /* 'sd_domain' column name */
-extern char* new_uri_column;   /* 'new_uri' column name */
-extern int   use_domain;      /* use or not the domain for sd lookup */
-extern str   dstrip_s;
+extern char* uid_column; 
+extern char* dial_username_column;
+extern char* dial_did_column;
+extern char* new_uri_column;
 
 extern db_func_t db_funcs;    /* Database functions */
 extern db_con_t* db_handle;   /* Database connection handle */
