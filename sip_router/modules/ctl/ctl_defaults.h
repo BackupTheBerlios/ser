d8554ca0459b645fdb60a33e6a9453501183b679
andrei <andrei>
2006-02-23 19:57:31 +0000

    - new fifo/unixsock/xmlrpc like module (implements the rpc api):   - supports multiple simultaneous connections over tcp, udp  and unix     datagram or stream sockets   - uses a space efficient binary encoding (for details see the top     comments in modules/ctl/binrpc.h)   - fast   - forks only one extra process   - supports also the old fifo protocol, extended to work also on     tcp, udp and unix sockets (you could use fifo over telnet for example)   - it doesn't depend on any external library  see modules/ctl/ctl.cfg for an example config and possible module parameters.

---
+++ sip_router/modules/ctl/ctl_defaults.h
@@ -0,0 +1,11 @@
+/* $Id: ctl_defaults.h,v 1.1 2006/02/23 19:57:31 andrei Exp $
+ */
+
+#ifndef __ctl_defaults_h
+#define __ctl_defaults_h
+/*listen by default on: */
+#define DEFAULT_CTL_SOCKET  "unixs:/tmp/ser_ctl"
+/* port used by default for tcp/udp if no port is explicitely specified */
+#define DEFAULT_CTL_PORT 2049
+
+#endif
