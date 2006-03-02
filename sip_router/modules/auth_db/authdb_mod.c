be4988b60a0c5ca6c0f8871e07c345f2c77752ce
kubartv <kubartv>
2006-03-02 13:56:10 +0000

    returned lost auth_dbf initialization

--- sip_router/modules/auth_db/authdb_mod.c
+++ sip_router/modules/auth_db/authdb_mod.c
@@ -1,5 +1,5 @@
 /*
- * $Id: authdb_mod.c,v 1.31 2006/03/01 16:01:13 janakj Exp $
+ * $Id: authdb_mod.c,v 1.32 2006/03/02 13:56:10 kubartv Exp $
  *
  * Digest Authentication Module
  *
@@ -175,7 +175,10 @@ static int mod_init(void)
 	bind_sl_t bind_sl;
 
 	DBG("auth_db module - initializing\n");
-
+	if (bind_dbmod(db_url, &auth_dbf) < 0){
+		LOG(L_ERR, "auth_db:child_init: Unable to bind a database driver\n");
+		return -2;
+	}
         bind_sl = (bind_sl_t)find_export("bind_sl", 0, 0);
 	if (!bind_sl) {
 		ERR("This module requires sl module\n");
