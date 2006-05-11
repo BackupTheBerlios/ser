92d4d168dcca475c16b9d56f30158dee437ff6e1
calrissian <calrissian>
2006-05-11 13:06:06 +0000

    made received uri parameter IPv6 compliant

--- sip_router/modules/nathelper/nathelper.c
+++ sip_router/modules/nathelper/nathelper.c
@@ -1,4 +1,4 @@
-/* $Id: nathelper.c,v 1.95 2006/02/20 16:17:36 janakj Exp $
+/* $Id: nathelper.c,v 1.96 2006/05/11 13:06:06 calrissian Exp $
  *
  * Copyright (C) 2003 Porta Software Ltd
  *
@@ -2051,8 +2051,12 @@ create_rcv_uri(str* uri, struct sip_msg* m)
 	memcpy(p, "sip:", 4);
 	p += 4;
 
+	if (m->rcv.src_ip.af == AF_INET6)
+		*p++ = '[';
 	memcpy(p, src_ip.s, src_ip.len);
 	p += src_ip.len;
+	if (m->rcv.src_ip.af == AF_INET6)
+		*p++ = ']';
 
 	*p++ = ':';
 
