ffd1c0e4dbb087ffc1726e3badfb50f0fbf5f1ad
andrei <andrei>
2007-03-23 18:21:50 +0000

    - epxerimental TMCB_LOCAL_REQUEST_IN callback: must be registered from mod_init (before forking) and is called each time a new local transaction is created  (e.g. t_uac called from fifo/rpc).

--- sip_router/modules/tm/uac.c
+++ sip_router/modules/tm/uac.c
@@ -1,5 +1,5 @@
 /*
- * $Id: uac.c,v 1.66 2007/03/15 16:30:16 andrei Exp $
+ * $Id: uac.c,v 1.67 2007/03/23 18:21:50 andrei Exp $
  *
  * simple UAC for things such as SUBSCRIBE or SMS gateway;
  * no authentication and other UAC features -- just send
@@ -54,6 +54,7 @@
  *               fixes 0 fr_timer bug (andrei)
  *  2006-08-11  t_uac uses dns failover until it finds a send socket (andrei)
  *  2007-03-15  TMCB_ONSEND callbacks support added (andrei)
+ *  2007-03-23  TMCB_LOCAL_REQUEST_IN callbacks support (andrei)
  */
 
 #include <string.h>
@@ -294,6 +295,8 @@ static inline int t_uac_prepare(str* method, str* headers, str* body,
 	request->buffer_len = buf_len;
 	new_cell->nr_of_outgoings++;
 	
+	if (has_local_reqin_tmcbs())
+			run_local_reqin_callbacks(new_cell, 0, 0);
 	if (dst_req) *dst_req = request;
 	if (dst_cell) *dst_cell = new_cell;
 	
