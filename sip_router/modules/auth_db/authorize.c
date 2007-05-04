aae3bc052f94249ad313c2c9ef05fe5fe070c93b
sobomax <sobomax>
2007-05-04 21:35:52 +0000

    Add support for loading non-string (int only at this time) fields via load_credentials feature. For now, they are converted into the string AVP and as such should be either processed as strings or converted into integers using avpops module functions. Non-ideal solution, but it's better than crashing when trying to access invalid pointer.

--- sip_router/modules/auth_db/authorize.c
+++ sip_router/modules/auth_db/authorize.c
@@ -1,5 +1,5 @@
 /*
- * $Id: authorize.c,v 1.46 2007/04/18 13:08:46 janakj Exp $
+ * $Id: authorize.c,v 1.47 2007/05/04 21:35:52 sobomax Exp $
  *
  * Digest Authentication - Database support
  *
@@ -35,6 +35,8 @@
  */
 
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "../../ut.h"
 #include "../../str.h"
@@ -176,13 +178,31 @@ static int generate_avps(db_res_t* result, db_rec_t *row)
     int i;
     int_str iname, ivalue;
     str value;
+    char buf[32];
     
 	for (i = 2; i < credentials_n + 2; i++) {
 		value = row->fld[i].v.lstr;
 
-		if (IS_NULL(row->fld[i]) || value.s == NULL) {
+		if (IS_NULL(row->fld[i]))
 			continue;
+
+		switch (row->fld[i].type) {
+		case DB_STR:
+		    value = row->fld[i].v.lstr;
+		    break;
+
+		case DB_INT:
+		    value.len = sprintf(buf, "%d", row->fld[i].v.int4);
+		    value.s = buf;
+		    break;
+
+		default:
+		    abort();
+		    break;
 		}
+
+		if (value.s == NULL)
+		    continue;
 
 		iname.s = credentials[i - 2];
 		ivalue.s = value;
