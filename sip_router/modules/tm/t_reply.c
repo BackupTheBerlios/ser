0afc02af6253d31fc963d90a8f39dba5445e3e31
andrei <andrei>
2007-05-29 13:44:19 +0000

    - added membar_depends() for run_callbacks and unmatched_totag - callbacks ids are no longer set: nobody uses them and when doing  a lockeless insert they cause extra expensive writes

--- sip_router/modules/tm/t_reply.c
+++ sip_router/modules/tm/t_reply.c
@@ -1,5 +1,5 @@
 /*
- * $Id: t_reply.c,v 1.138 2007/05/29 09:36:51 andrei Exp $
+ * $Id: t_reply.c,v 1.139 2007/05/29 13:44:19 andrei Exp $
  *
  *
  * Copyright (C) 2001-2003 FhG Fokus
@@ -225,7 +225,10 @@ int unmatched_totag(struct cell *t, struct sip_msg *ack)
 		return 1;
 	}
 	tag=&get_to(ack)->tag_value;
-	for (i=t->fwded_totags; i; i=i->next) {
+	i=t->fwded_totags;
+	while(i){
+		membar_depends(); /* make sure we don't see some old i content
+							(needed on CPUs like Alpha) */
 		if (i->tag.len==tag->len
 				&& memcmp(i->tag.s, tag->s, tag->len)==0) {
 			DBG("DEBUG: totag for e2e ACK found: %d\n", i->acked);
@@ -235,6 +238,7 @@ int unmatched_totag(struct cell *t, struct sip_msg *ack)
 			i->acked=1;
 			return 1;
 		}
+		i=i->next;
 	}
 	/* surprising: to-tag never sighted before */
 	return 1;
