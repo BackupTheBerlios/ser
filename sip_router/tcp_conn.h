f111ade3f309179020af6eaff46d75ed0afde3db
andrei <andrei>
2007-12-12 19:11:25 +0000

    - tcp: close connection immediately if the write buf. timeouts (timeout fixes)  + some cleanups

--- sip_router/tcp_conn.h
+++ sip_router/tcp_conn.h
@@ -1,5 +1,5 @@
 /*
- * $Id: tcp_conn.h,v 1.34 2007/12/04 20:25:29 andrei Exp $
+ * $Id: tcp_conn.h,v 1.35 2007/12/12 19:11:25 andrei Exp $
  *
  * Copyright (C) 2001-2003 FhG Fokus
  *
@@ -135,6 +135,7 @@ struct tcp_conn_alias{
 	struct tcp_wbuffer_queue{
 		struct tcp_wbuffer* first;
 		struct tcp_wbuffer* last;
+		ticks_t wr_timeout; /* write timeout*/
 		unsigned int queued; /* total size */
 		unsigned int offset; /* offset in the first wbuffer were data
 								starts */
@@ -167,7 +168,6 @@ struct tcp_connection{
 	struct tcp_conn_alias con_aliases[TCP_CON_MAX_ALIASES];
 	int aliases; /* aliases number, at least 1 */
 #ifdef TCP_BUF_WRITE
-	ticks_t last_write; /* time when the last write took place */
 	struct tcp_wbuffer_queue wbuf_q;
 #endif
 };
