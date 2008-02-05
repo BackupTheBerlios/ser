2671e2a43b5513eed45b85a9122a7227feaea92a
andrei <andrei>
2008-02-05 21:47:29 +0000

    - avoid extra read syscalls (detect short reads) in poll_et or sigio_rt case  (should improve performace) - if POLLRDHUP or EPOLLRDHUP are supported (linux >= 2.6.17) use them  and avoid an extra syscall on EOF - on write error try to see if there's still some data in the socket read  buffer and try to process it first (if there's no more data do a quick   connection destroy)

--- sip_router/tcp_conn.h
+++ sip_router/tcp_conn.h
@@ -1,5 +1,5 @@
 /*
- * $Id: tcp_conn.h,v 1.37 2007/12/21 17:58:07 andrei Exp $
+ * $Id: tcp_conn.h,v 1.38 2008/02/05 21:47:29 andrei Exp $
  *
  * Copyright (C) 2001-2003 FhG Fokus
  *
@@ -75,6 +75,10 @@
 #define F_CONN_FD_CLOSED   32 /* fd was already closed */
 #define F_CONN_PENDING     64 /* pending connect  (fd not known yet in main) */
 #define F_CONN_MAIN_TIMER 128 /* timer active in the tcp_main process */
+#define F_CONN_EOF_SEEN   256 /* FIN or RST have been received */
+#define F_CONN_FORCE_EOF  512 /* act as if an EOF was received */
+#define F_CONN_OOB_DATA  1024 /* out of band data on the connection */
+#define F_CONN_WR_ERROR  2048 /* write error on the fd */
 
 
 enum tcp_req_errors {	TCP_REQ_INIT, TCP_REQ_OK, TCP_READ_ERROR,
