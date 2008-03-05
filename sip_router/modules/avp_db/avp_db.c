77d2a97150881b24e47d91dc89e73504de94fe94
janakj <janakj>
2008-03-05 07:59:19 +0000

    - an error message that is too noisy turned into debugging message

--- sip_router/modules/avp_db/avp_db.c
+++ sip_router/modules/avp_db/avp_db.c
@@ -1,5 +1,5 @@
 /*
- * $Id: avp_db.c,v 1.32 2007/06/25 17:51:30 liborc Exp $
+ * $Id: avp_db.c,v 1.33 2008/03/05 07:59:19 janakj Exp $
  *
  * Copyright (C) 2004 FhG Fokus
  *
@@ -257,7 +257,7 @@ static int load_uri_attrs(struct sip_msg* msg, unsigned long flags, fparam_t* fp
 	static str default_did = STR_STATIC_INIT(DEFAULT_DID);
 
 	if (get_str_fparam(&uri, msg, (fparam_t*)fp) != 0) {
-		ERR("Unable to get URI\n");
+		DBG("Unable to get URI from load_uri_attrs parameters\n");
 		return -1;
 	}
 
@@ -300,7 +300,7 @@ static int load_user_attrs(struct sip_msg* msg, unsigned long flags, fparam_t* f
     db_res_t* res;
 
 	if (get_str_fparam(&load_user_attrs_cmd->match[0].v.lstr, msg, (fparam_t*)fp) < 0) {
-		ERR("Unable to get UID\n");
+		DBG("Unable to get UID from load_user_attrs parameter\n");
 		return -1;
 	}
 
