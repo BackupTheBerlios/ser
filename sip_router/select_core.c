26d8a967add9a91d0772246231997cc01cd2948a
tirpi <tirpi>
2008-05-22 16:15:07 +0000

    @msg.body.sdp select call is introduced, that returns the sdp part of the message body.

--- sip_router/select_core.c
+++ sip_router/select_core.c
@@ -1,5 +1,5 @@
 /*
- * $Id: select_core.c,v 1.31 2008/05/13 09:07:18 janakj Exp $
+ * $Id: select_core.c,v 1.32 2008/05/22 16:15:07 tirpi Exp $
  *
  * Copyright (C) 2005-2006 iptelorg GmbH
  *
@@ -57,6 +57,8 @@
 #include "parser/parse_expires.h"
 #include "parser/parse_refer_to.h"
 #include "parser/parse_rpid.h"
+#include "parser/parse_content.h"
+#include "parser/parse_body.h"
 #include "dset.h"
 #include "sr_module.h"
 
@@ -492,6 +494,18 @@ int select_msg_body(str* res, select_t* s, struct sip_msg* msg)
 	res->s = get_body(msg);
 	res->len = msg->buf+msg->len - res->s;
 	return 0;	
+}
+
+int select_msg_body_sdp(str* res, select_t* sel, struct sip_msg* msg)
+{
+	/* try to get the body part with application/sdp */
+	if ((res->s = get_body_part(msg,
+				TYPE_APPLICATION, SUBTYPE_SDP,
+				&res->len))
+	)
+		return 0;
+	else
+		return -1;
 }
 
 int select_msg_header(str* res, select_t* s, struct sip_msg* msg)
