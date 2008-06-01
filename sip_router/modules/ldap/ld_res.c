3210faea11af85890115de82c42da294a4fa03f2
janakj <janakj>
2008-06-01 09:35:48 +0000

    - license text reformatted - changed header field order in ld_uri.c

--- sip_router/modules/ldap/ld_res.c
+++ sip_router/modules/ldap/ld_res.c
@@ -1,5 +1,5 @@
 /* 
- * $Id: ld_res.c,v 1.2 2008/05/29 15:17:49 gkovacs Exp $ 
+ * $Id: ld_res.c,v 1.3 2008/06/01 09:35:48 janakj Exp $ 
  *
  * LDAP Database Driver for SER
  *
@@ -10,11 +10,7 @@
  * SER is free software; you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
- * version
- *
- * For a license to use the ser software under conditions other than those
- * described here, or to purchase support for this software, please contact
- * iptel.org by e-mail at the following addresses: info@iptel.org
+ * version.
  *
  * SER is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
@@ -22,8 +18,8 @@
  * details.
  *
  * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc., 59
- * Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * with this program; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 /** \addtogroup ldap
