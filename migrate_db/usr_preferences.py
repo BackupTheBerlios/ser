c23fc318702d473c05488310a57139c48e3b4a6e
janakj <janakj>
2008-08-26 12:12:56 +0000

    Added missing formatting character

--- migrate_db/usr_preferences.py
+++ migrate_db/usr_preferences.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2.3
 #
-# $Id: usr_preferences.py,v 1.2 2008/08/26 10:58:06 janakj Exp $
+# $Id: usr_preferences.py,v 1.3 2008/08/26 12:12:56 janakj Exp $
 #
 # Copyright (C) 2006 iptelorg GmbH
 #
@@ -41,7 +41,7 @@ def convert_usr_preferences(dst, src):
         try:
             uid = get_uid_by_uri(dst, username, domain)
             dst.cursor().execute("insert into user_attrs (uid, name, type, value, flags) values "
-                                 "(%s, %s, %s, %s, %)", (uid, attribute, type, value, common.DB_LOAD_SER | common.DB_FOR_SERWEB))
+                                 "(%s, %s, %s, %s, %s)", (uid, attribute, type, value, common.DB_LOAD_SER | common.DB_FOR_SERWEB))
         except UidNotFoundException:
             print "ERROR: Cannot find UID user_preferences entry for '%s@%s'" % (username, domain)
 
