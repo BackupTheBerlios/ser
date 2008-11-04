162735f1987cc35f0a9f2ecc18611bc2c8bd386d
sobomax <sobomax>
2008-11-04 11:11:58 +0000

    Move helper functions out from overly long nathelper.c.

--- sip_router/modules/nathelper/nhelpr_funcs.h
+++ sip_router/modules/nathelper/nhelpr_funcs.h
@@ -1,5 +1,5 @@
 /*
- * $Id: nhelpr_funcs.h,v 1.3 2004/08/24 08:58:32 janakj Exp $
+ * $Id: nhelpr_funcs.h,v 1.4 2008/11/04 11:11:58 sobomax Exp $
  *
  *
  * Copyright (C) 2001-2003 FhG Fokus
@@ -32,8 +32,14 @@
 
 #include "../../str.h"
 #include "../../parser/msg_parser.h"
+#include "../../parser/contact/contact.h"
 
 int extract_body(struct sip_msg * , str *);
 int check_content_type(struct sip_msg * );
+void *ser_memmem(const void *, const void *, size_t, size_t);
+int get_callid(struct sip_msg *, str *);
+int get_to_tag(struct sip_msg *, str *);
+int get_from_tag(struct sip_msg *, str *);
+int get_contact_uri(struct sip_msg *, struct sip_uri *, contact_t **);
 
 #endif
