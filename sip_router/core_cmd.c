b7980098c9631f1adac86be78123328b7a33b4f0
andrei <andrei>
2009-06-10 23:23:52 +0000

    core: sctp_options rpc updated
    Updated sctp_options with the new options (asocmaxrxt,
    init_max_attempts, init_max_timeo, hbinterval, pathmaxrxt,
    sack_delay, sack_freq, max_burst).
    
    E.g.:
    $ sercmd core.sctp_options
    {
    	sctp_socket_rcvbuf: 54784
    	sctp_socket_sndbuf: 54784
    	sctp_autoclose: 180
    	sctp_send_ttl: 32000
    	sctp_send_retries: 0
    	sctp_srto_initial: 3000
    	sctp_srto_max: 60000
    	sctp_srto_min: 1000
    	sctp_asocmaxrxt: 10
    	sctp_init_max_attempts: 8
    	sctp_init_max_timeo: 60000
    	sctp_hbinterval: 30000
    	sctp_pathmaxrxt: 5
    	sctp_sack_delay: 200
    	sctp_sack_freq: 0
    	sctp_max_burst: 4
    }

--- sip_router/core_cmd.c
+++ sip_router/core_cmd.c
@@ -1,5 +1,5 @@
 /*
- * $Id: core_cmd.c,v 1.38 2009/05/29 12:24:58 andrei Exp $
+ * $Id: core_cmd.c,v 1.39 2009/06/10 23:23:52 andrei Exp $
  *
  * Copyright (C) 2005 iptelorg GmbH
  *
@@ -613,7 +613,7 @@ static void core_sctp_options(rpc_t* rpc, void* c)
 	if (!sctp_disable){
 		sctp_options_get(&t);
 		rpc->add(c, "{", &handle);
-		rpc->struct_add(handle, "dddddddd",
+		rpc->struct_add(handle, "dddddddddddddddd",
 			"sctp_socket_rcvbuf",	t.so_rcvbuf,
 			"sctp_socket_sndbuf",	t.so_sndbuf,
 			"sctp_autoclose",		t.autoclose,
@@ -621,7 +621,15 @@ static void core_sctp_options(rpc_t* rpc, void* c)
 			"sctp_send_retries",	t.send_retries,
 			"sctp_srto_initial",	t.srto_initial,
 			"sctp_srto_max",		t.srto_max,
-			"sctp_srto_min",		t.srto_min
+			"sctp_srto_min",		t.srto_min,
+			"sctp_asocmaxrxt",	t.asocmaxrxt,
+			"sctp_init_max_attempts",	t.init_max_attempts,
+			"sctp_init_max_timeo",t.init_max_timeo,
+			"sctp_hbinterval",	t.hbinterval,
+			"sctp_pathmaxrxt",	t.pathmaxrxt,
+			"sctp_sack_delay",	t.sack_delay,
+			"sctp_sack_freq",	t.sack_freq,
+			"sctp_max_burst",	t.max_burst
 		);
 	}else{
 		rpc->fault(c, 500, "sctp support disabled");
